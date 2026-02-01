88c25a9843cf85949da1c292d029b937511bda94
Satwik Ghildiyal <satwik15.g@gmail.com>
2026-02-01 23:32:17 +0530

    deposit test written and passing

--- tests/anchor-amm-q4-25.ts
+++ tests/anchor-amm-q4-25.ts
@@ -2,7 +2,7 @@ import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorAmmQ425 } from "../target/types/anchor_amm_q4_25";
 import { ASSOCIATED_TOKEN_PROGRAM_ID, createAssociatedTokenAccountInstruction,
-  createMint, getAssociatedTokenAddressSync, mintTo, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+  createMint, getAssociatedTokenAddressSync, mintTo, TOKEN_PROGRAM_ID, getAccount } from "@solana/spl-token";
 import { expect } from "chai";
 
 describe("anchor-amm-q4-25", () => {
@@ -24,9 +24,9 @@ describe("anchor-amm-q4-25", () => {
 
   let mintY: anchor.web3.PublicKey;
 
-  // let depositorATAX: anchor.web3.PublicKey;
+  let depositorATAX: anchor.web3.PublicKey;
 
-  // let depositorATAY: anchor.web3.PublicKey;
+  let depositorATAY: anchor.web3.PublicKey;
 
   let vaultX: anchor.web3.PublicKey;
 
@@ -40,9 +40,9 @@ describe("anchor-amm-q4-25", () => {
 
   let configBump: number;
 
-  const depositAmount = 10;
+  const depositAmount = 100;
 
-  // let depositorMintLPATA: anchor.web3.PublicKey;
+  let depositorMintLPATA: anchor.web3.PublicKey;
 
   before(async () => {
 
@@ -66,25 +66,39 @@ describe("anchor-amm-q4-25", () => {
 
     console.log(`Address of the LP mint is: ${mintLP.toBase58()}`);
 
-    mintX = await createMint(provider.connection, provider.wallet.payer, depositor.publicKey, null, 6);
+    depositorMintLPATA = getAssociatedTokenAddressSync(mintLP, depositor.publicKey, true);
 
-    mintY = await createMint(provider.connection, provider.wallet.payer, depositor.publicKey, null, 6);
+    mintX = await createMint(provider.connection, provider.wallet.payer, provider.wallet.publicKey, null, 6);
+
+    mintY = await createMint(provider.connection, provider.wallet.payer, provider.wallet.publicKey, null, 6);
 
     vaultX = getAssociatedTokenAddressSync(mintX, configPDA, true);
 
     vaultY = getAssociatedTokenAddressSync(mintY, configPDA, true);
 
-    //depositorATAX = getAssociatedTokenAddressSync(mintX, depositor.publicKey);
+    depositorATAX = getAssociatedTokenAddressSync(mintX, depositor.publicKey);
 
-    // const depositorATAXTx = new anchor.web3.Transaction().add(
+    const depositorATAXTx = new anchor.web3.Transaction().add(
 
-    //   createAssociatedTokenAccountInstruction(provider.wallet.publicKey, depositorATAX, depositor.publicKey, mintX)
+      createAssociatedTokenAccountInstruction(provider.wallet.publicKey, depositorATAX, depositor.publicKey, mintX)
     
-    // );
+    );
 
-    // await provider.sendAndConfirm(depositorATAXTx);
+    await provider.sendAndConfirm(depositorATAXTx);
 
-    //await mintTo(provider.connection, provider.wallet.payer, mintX, depositorATAX, provider.wallet.payer, BigInt(depositAmount) * BigInt(10 ** 6));
+    await mintTo(provider.connection, provider.wallet.payer, mintX, depositorATAX, provider.wallet.payer, BigInt(depositAmount) * BigInt(10 ** 6));
+
+    depositorATAY = getAssociatedTokenAddressSync(mintY, depositor.publicKey);
+
+    const depositorATAYTx = new anchor.web3.Transaction().add(
+
+      createAssociatedTokenAccountInstruction(provider.wallet.publicKey, depositorATAY, depositor.publicKey, mintY)
+    
+    );
+
+    await provider.sendAndConfirm(depositorATAYTx);
+
+    await mintTo(provider.connection, provider.wallet.payer, mintY, depositorATAY, provider.wallet.payer, BigInt(depositAmount) * BigInt(10 ** 6));
 
 
   });
@@ -119,5 +133,36 @@ describe("anchor-amm-q4-25", () => {
     expect(configAccount.mintX.toBase58()).to.equal(mintX.toBase58());
 
     expect(configAccount.mintY.toBase58()).to.equal(mintY.toBase58());
+  
   });
+
+  
+  it("Liquidity deposited!", async () => {
+
+    const tx = await program.methods.deposit(new anchor.BN(10 * (10 ** 6)), new anchor.BN(100 * (10 ** 6)), new anchor.BN(100 * (10 ** 6))).accountsStrict({
+
+      user: depositor.publicKey,
+      mintX: mintX,
+      mintY: mintY,
+      config: configPDA,
+      mintLp: mintLP,
+      vaultX: vaultX,
+      vaultY: vaultY,
+      userX: depositorATAX,
+      userY: depositorATAY,
+      userLp: depositorMintLPATA,
+      tokenProgram: TOKEN_PROGRAM_ID,
+      associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      systemProgram: anchor.web3.SystemProgram.programId
+
+    }).signers([depositor]).rpc();
+
+    console.log("Your transaction signature", tx);
+
+    const depositorMintLPATAAccount = await getAccount(provider.connection, depositorMintLPATA);
+
+    expect(depositorMintLPATAAccount.amount).to.equal(BigInt(10) * BigInt(10 ** 6));
+
+  });
+
 });
